37a64c82cca586fbbb29dcaad350b937b1fcfce0
Daniel Tolnai <danielz.tolnai@gmail.com>
2016-06-12 14:58:16 +0200

    Modify bitmapbank to use BRAM and read initial values from file

--- bitmapbank.v
+++ bitmapbank.v
@@ -7,49 +7,23 @@ module bitmapbank #(
    input             clk,
    input  [SIZE-1:0] addr,   // Start address (16x16 blocks)
    input             width,  // Number of 16 blocks horizontally (0-1)
-   input             height, // Number of 16 blocks vertically (0-1)
    input  [4:0]      hpos,   // Horizontal poistion in bitmap (0-31)
    input  [4:0]      vpos,   // Vertical poistion in bitmap (0-31)
    output [5:0]      pixel   // Output pixel (RRGGBB)
 );
 
-wire [3:0] bhpos, bvpos;
-assign bhpos = hpos[3:0];
-assign bvpos = vpos[3:0];
+reg [5:0] bitmap_array[((2**SIZE)*256)-1:0];
+initial $readmemh("bitmap.txt", bitmap_array);
 
-reg [5:0] bitmap_array[(2**SIZE)-1:0][15:0][15:0];
+wire [8+SIZE:0] memaddr;
+//                addr *256  +   vpos * 16  *(width+1) + hpos
+assign memaddr = (addr << 8) + ((vpos << 4) <<  width) + hpos;
 
-/*
- * width==0 || height==0 -> addr = addr + hpos[4] + vpos[4]           // top/left | bottom/right
- * width==1 && height==1 -> addr = addr + hpos[4] + vpos[4] + vpos[4] // top left | top right | bottom left | bottom right
- */
-wire [SIZE-1:0] real_addr;
-assign real_addr = ((width & height) == 0) ? (addr + hpos[4] + vpos[4]) : (addr + hpos[4] + vpos[4] + vpos[4]);
-
-assign pixel = bitmap_array[real_addr][bvpos][bhpos];
-
-integer i, j, k;
-initial
+reg [5:0] pixelReg;
+always@(posedge clk)
 begin
-for (i=0; i<4; i=i+1)
-   for (j=0; j<16; j=j+1)
-      for (k=0; k<16; k=k+1)
-         bitmap_array[i][j][k] = 6'b001100;
-		
-for (i=4; i<8; i=i+1)
-   for (j=0; j<16; j=j+1)
-      for (k=0; k<16; k=k+1)
-         bitmap_array[i][j][k] = 6'b000011;
-		
-for (i=8; i<12; i=i+1)
-   for (j=0; j<16; j=j+1)
-      for (k=0; k<16; k=k+1)
-         bitmap_array[i][j][k] = 6'b001111;
-
-for (i=12; i<16; i=i+1)
-   for (j=0; j<16; j=j+1)
-      for (k=0; k<16; k=k+1)
-         bitmap_array[i][j][k] = 6'b111001;
+   pixelReg <= bitmap_array[memaddr];
 end
+assign pixel = pixelReg;
 
 endmodule
